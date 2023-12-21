0e3ecafc641cc3787855111663d137babfb4da3f
Samay001 <samayrathod1@gmail.com>
2023-12-22 05:19:06 +0530

    navbar setup

--- client/src/components/layout/footer.js
+++ client/src/components/layout/footer.js
@@ -1,13 +1,66 @@
 import React from "react";
+import { NavLink } from 'react-router-dom';
+import payment from './payment.png';
+import logo from './logo.png';
+import './Footer.css';
 
 const Footer = () => {
   return (
-    <footer>
-        <div className="bg-dark text-light p-3">
-            <h4 className="text-center">All Right Reserverd Urbansnitch</h4>
+    <footer id="footer-main">
+        <div className="col-flex">
+          <NavLink className="navbar-brand" to="/">
+            <img src={logo} alt="Urbansnitch Logo" className="logo-img" />
+          </NavLink>
+          <h4>Contact</h4>
+          <p><strong>Address:</strong> E-83,Sector-1,Noida</p>
+          <p><strong>Phone:</strong> 0120-123456</p>
+          <p><strong>Email:</strong> urbansnitch_support@gmail.com </p>
+          <div className="social-handles">
+            <h4>Follow Us</h4>
+            <div className="social-icons">
+              <i className="fab fa-facebook-f fa-lg" />
+              <i className="fab fa-instagram fa-lg" />
+              <i className="fab fa-twitter fa-lg" />
+              <i className="fab fa-youtube fa-lg" />
+            </div>  
+          </div>
+        </div>
+        <div className="col-flex">
+          <h4>About</h4>
+          <ul>
+            <li><NavLink to="/" href>About Us</NavLink></li>
+            <li><NavLink to="/" href>Careers</NavLink></li>
+            <li><NavLink to="/" href>Press</NavLink></li>
+            <li><NavLink to="/" href>Blog</NavLink></li>
+          </ul>
+        </div>
+        <div className="col-flex">
+          <h4>My Account</h4>
+          <ul>
+            <li><NavLink to="/" href>Sign in</NavLink></li>
+            <li><NavLink to="/" href>View Cart</NavLink></li>
+            <li><NavLink to="/" href>My wishlist</NavLink></li>
+            <li><NavLink to="/" href>Help</NavLink></li>
+          </ul>
+        </div>
+        <div className="col-flex">
+          <h4>Install App</h4>
+          <p>From Google Play or Apple Store</p>
+          <div className="app-logo">
+            <img src="/images/play.jpg" alt="" />
+            <img src="/images/app.jpg" alt="" />
+          </div>
+          <p>Secured Payment Gateways</p> 
+          <NavLink className="navbar-brand" to="/">
+            <img src={payment} alt="payment Logo" className="payment-img" />
+          </NavLink>
         </div>
     </footer>
   );
 }
 
 export default Footer;
+
+
+
+      
--- client/src/components/layout/header.js
+++ client/src/components/layout/header.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from './logo.png';
+import './Header.css';
 
-const Header = () => {
+function Header() {
   return (
-    <>
-        <header>
-            <h1>Header</h1>
-        </header>
-    </>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <NavLink className="navbar-brand" to="/">
+            <img src={logo} alt="Urbansnitch Logo" className="logo-img" />
+          </NavLink>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon" />
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <NavLink to="/" className="nav-link active" aria-current="page" href="#">Home</NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink to="/register" className="nav-link" href="#">Register</NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink to="/login" className="nav-link" href="#">Login</NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink to="/cart" className="nav-link" href="#">Cart(0)</NavLink>
+                </li>
+              </ul>
+            </div>
+        </div>
+      </nav>
   );
-};
+}
 
 export default Header;
+
+  
